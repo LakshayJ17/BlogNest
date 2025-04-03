bee2600351588c99532b3c4b113ee373ce57ed61
lakshay17 <lakshayjain01lj@gmail.com>
2025-04-03 13:06:21 +0530

    Added responsivness

--- frontend/src/components/FullBlog.tsx
+++ frontend/src/components/FullBlog.tsx
@@ -1,38 +1,48 @@
-import { Blog } from "../hooks"
-import { Appbar } from "./Appbar"
-import { Avatar } from "./BlogCard"
+import { Blog } from "../hooks";
+import { Appbar } from "./Appbar";
+import { Avatar } from "./BlogCard";
 
 export const FullBlog = ({ blog }: { blog: Blog }) => {
-    return <div>
-        <Appbar />
-        <div className="flex justify-center ">
-            <div className="grid grid-cols-12 px-10 w-full max-w-screen-xl pt-12">
-                <div className="col-span-8">
-                    <div className="text-5xl font-extrabold">{blog.title}</div>
-                    <div className="text-slate-500 pt-5">Posted on 2nd dec 20203</div>
-                    <div className="pt-4 ">{blog.content}</div>
-                </div>
-                <div className="col-span-4">
-                    <div className="text-slate-600 text-lg">
-                    Author
+    return (
+        <div>
+            <Appbar />
+            <div className="flex justify-center px-4 py-6">
+                <div className="grid grid-cols-1 lg:grid-cols-12 gap-6 w-full max-w-5xl">
+                    {/* Blog Content */}
+                    <div className="lg:col-span-8">
+                        <div className="text-3xl sm:text-4xl md:text-5xl font-extrabold">
+                            {blog.title}
+                        </div>
+                        <div className="text-slate-500 pt-3 sm:pt-5 text-sm sm:text-base">
+                            Posted on 2nd Dec 2023
+                        </div>
+                        <div className="pt-4 text-base sm:text-lg leading-relaxed">
+                            {blog.content}
+                        </div>
                     </div>
-                    
-                    <div className="flex w-full">
-                        <div className="pr-4 flex flex-col justify-center">
-                            <Avatar name={blog.author.name || "Anonymous"} size="big" />
-                        </div>
-                        <div>
-                            <div className="text-xl font-bold">
-                                {blog.author.name || "Anonymous"}
-                            </div>
-                            <div className="pt-2 text-slate-500">Random catchphrase hahahahahahhaha</div>
-                        </div>
 
+                    {/* Author Section */}
+                    <div className="lg:col-span-4">
+                        <div className="text-slate-600 text-lg sm:text-xl">
+                            Author
+                        </div>
+                        <div className="flex items-start gap-4 pt-4">
+                            <div>
+                                <Avatar name={blog.author.name || "Anonymous"} size="big" />
+                            </div>
+
+                            <div>
+                                <div className="text-xl font-bold">
+                                    {blog.author.name || "Anonymous"}
+                                </div>
+                                <div className="pt-2 text-slate-500 text-sm sm:text-base">
+                                    Random catchphrase about why he is better than rest of others and what is special about the author. Hahaha
+                                </div>
+                            </div>
+                        </div>
                     </div>
                 </div>
             </div>
         </div>
-
-    </div>
-
-}
+    );
+};
