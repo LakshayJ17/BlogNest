d7eac60c3999fde38274762eda3e021b94fe4070
lakshay17 <lakshayjain01lj@gmail.com>
2025-04-03 19:08:45 +0530

    updated fe

--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -175,7 +175,6 @@ export default function Home() {
             <div className="flex justify-center">
               <button
                 type="button"
-                onClick={() => navigate("/blogs")}
                 className="cursor-pointer text-gray-900 bg-white border border-gray-300 focus:outline-none hover:bg-gray-100 focus:ring-4 focus:ring-gray-100 font-medium rounded-lg text-sm px-5 py-2.5 inline-flex items-center gap-2"
               >
                 See more articles
