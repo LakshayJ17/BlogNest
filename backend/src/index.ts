a5ca96edc1eb38977a5f417ec2a0add588f7be0e
lakshay17 <lakshayjain01lj@gmail.com>
2026-01-11 09:07:48 +0530

    cors issue

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -10,7 +10,7 @@ dotenv.config();
 const app = express();
 
 app.use(cors({
-  origin: ["http://localhost:5173", "https://blognest-project.vercel.app"],
+  origin: ["http://localhost:5173", "https://blognest-project.vercel.app", "https://blognest.bylakshayjain.online"],
   credentials: true,
 }));
 app.use(express.json());
